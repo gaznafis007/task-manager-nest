bdaf919acc9f018cfb71804f0515100deee9887e
gaznafis007 <gazinafisrafi.gnr@gmail.com>
2025-06-09 18:18:00 +0600

    init: auth service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { TaskController } from './task/task.controller';
+import { TaskService } from './task/task.service';
 
 
 @Module({
   imports: [TasksModule, AuthModule],
   controllers: [TaskController],
+  providers: [TaskService],
 })
 export class AppModule {}
